f22ab60e1d3251c2bca60875b6f49ba207cc9192
cmatchneer <cmatchneer@yahoo.com>
2022-04-03 00:12:15 -0500

    navbar test worked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home-page";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h1>navbar test</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
